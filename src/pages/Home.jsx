860a3e09819ddc8cf676edd3849dd4f9dabab1f6
Nirmal-appiness <nirmal@appinessworld.com>
2025-07-15 23:41:06 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../layouts/Header'
+import { Footer } from '../layouts/Footer'
+
+function Home() {
+  return (
+    <div>
+        <Header/>
+         <div>
+            Body
+         </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
